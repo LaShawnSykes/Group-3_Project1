c4d81cd640175c95a100bc815fb9e371359b6618
RevHex <158772592+RevHex@users.noreply.github.com>
2024-04-18 21:42:46 -0500

    Format updates for PNG, Updated Doc, Updated Py Script

--- Code/visualizations.py
+++ Code/visualizations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[40]:
 
 
 # Importing necessary libraries
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from IPython.display import display
 
 
-# In[2]:
+# In[41]:
 
 
 clean_data = pd.read_excel('csdm.xlsx')
@@ -20,20 +20,20 @@ type1 = pd.read_excel('../Datasets/Shanghai_T1DM_Summary.xlsx')
 type2 = pd.read_excel('../Datasets/Shanghai_T2DM_Summary.xlsx')
 
 
-# In[3]:
+# In[42]:
 
 
 # Load the dataset
 cd1 = clean_data
 
 
-# In[4]:
+# In[43]:
 
 
 cd1 
 
 
-# In[5]:
+# In[44]:
 
 
 # Displaying the columns of cd1
@@ -42,24 +42,24 @@ print(cd1.columns)
 
 
 
-# In[6]:
+# In[45]:
 
 
 cd1.iloc[:, 7]
 
 
-# In[7]:
+# In[46]:
 
 
 print(cd1.dtypes)
 
 
-# In[8]:
+# In[47]:
 
 
 #Looking for coorelations between different columns. 
 #In this example its a comparison of age factor vs total cholesterol.
-
+plt.figure(figsize=(8, 6))
 plt.scatter(cd1.iloc[:,2],cd1.iloc[:,22])
 plt.xlabel('Age (years)')
 plt.ylabel('Total Cholesterol (mmol/L)')
@@ -70,7 +70,7 @@ plt.show()
 #Seems to be a average of 5mmol/L of Cholesterol in Diabetic Patients
 
 
-# In[9]:
+# In[48]:
 
 
 # Define mapping of merged categories to new labels for Microvascular Complications
@@ -92,7 +92,7 @@ cd1['Microvascular Complications'] = cd1['Diabetic Microvascular Complications']
 print(cd1['Microvascular Complications'].unique())
 
 
-# In[10]:
+# In[49]:
 
 
 # Define a function to merge and relabel the categories
@@ -109,7 +109,7 @@ cd1['Microvascular Complications'] = cd1['Microvascular Complications'].apply(me
 print(cd1['Microvascular Complications'].unique())
 
 
-# In[11]:
+# In[50]:
 
 
 # Body Mass Index (BMI) Category: A new feature based on BMI ranges.
@@ -127,13 +127,13 @@ def categorize_bmi(bmi):
 cd1['BMI Category'] = cd1['BMI (kg/m2)'].apply(categorize_bmi)
 
 
-# In[12]:
+# In[51]:
 
 
 #cd1['BMI Category'] = cd1['BMI Category'].apply(['Underweight', 'Normal Weight', 'Overweight', 'Obese'])
 
 
-# In[13]:
+# In[52]:
 
 
 #Total Daily Insulin Dose: Utilizing data on both fasting and postprandial insulin levels, we can calculate the total daily insulin dose:
@@ -144,7 +144,7 @@ cd1['Merge 2-hour Postprandial insulin (pmol/L)'] = pd.to_numeric(cd1['Merge 2-h
 cd1['Total Daily Insulin Dose (pmol/L)'] = (cd1['Fasting Insulin (pmol/L)'] + cd1['Merge 2-hour Postprandial insulin (pmol/L)']) * 24
 
 
-# In[14]:
+# In[53]:
 
 
 # Separating data for male and female patients
@@ -152,7 +152,7 @@ male_data = cd1[cd1['Gender (Female=1, Male=2)'] == 2]
 female_data = cd1[cd1['Gender (Female=1, Male=2)'] == 1]
 
 
-# In[15]:
+# In[54]:
 
 
 # Calculating mean BMI for male and female patients to show functional use of gendered data.
@@ -163,7 +163,7 @@ print("Mean BMI for male patients:", mean_bmi_male)
 print("Mean BMI for female patients:", mean_bmi_female)
 
 
-# In[16]:
+# In[55]:
 
 
 # Define the desired order of categories
@@ -173,7 +173,7 @@ desired_order = ['Underweight', 'Normal Weight', 'Overweight', 'Obese']
 cd1['BMI Category'] = pd.Categorical(cd1['BMI Category'], categories=desired_order, ordered=True)
 
 
-# In[17]:
+# In[56]:
 
 
 # Plotting BMI distribution
@@ -188,7 +188,7 @@ plt.savefig('bmidist.png')
 plt.show()
 
 
-# In[18]:
+# In[57]:
 
 
 # Plotting BMI distribution for male and female patients
@@ -212,7 +212,7 @@ plt.savefig('bmidistgender.png')
 plt.show()
 
 
-# In[19]:
+# In[58]:
 
 
 # Sort the data by 'Hypoglycemia (yes/no)'
@@ -220,11 +220,11 @@ cd1_sorted = cd1.sort_values(by='Hypoglycemia (yes/no)')
 cd1_sorted['Hypoglycemia (yes/no)'] = cd1_sorted['Hypoglycemia (yes/no)'].replace({'yes': 'Yes', 'no': 'No'})
 
 
-# In[20]:
+# In[59]:
 
 
 # Visualization 2: Bar Chart of Hypoglycemia Incidence
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(8, 6))
 sns.countplot(x='Hypoglycemia (yes/no)', data=cd1_sorted, order=['Yes', 'No'])
 plt.title('Hypoglycemia Incidence')
 plt.ylabel('Total Patients in Sample Set')
@@ -233,13 +233,13 @@ plt.savefig('hypoglycemia.png')
 plt.show()
 
 
-# In[21]:
+# In[60]:
 
 
 cd1['Diabetic Microvascular Complications'].unique()
 
 
-# In[22]:
+# In[61]:
 
 
 mapping = {
@@ -260,13 +260,13 @@ cd1['Remapped Microvascular Complications'] = cd1['Diabetic Microvascular Compli
 print(cd1['Remapped Microvascular Complications'].unique())
 
 
-# In[23]:
+# In[62]:
 
 
 cd1['Remapped Microvascular Complications'].value_counts()
 
 
-# In[24]:
+# In[63]:
 
 
 # Define a function to merge and relabel the categories
@@ -279,7 +279,7 @@ def merge_categories(category):
 # Apply the function to the 'Diabetic Microvascular Complications' column to create the merged column
 cd1['Microvascular Complications'] = cd1['Remapped Microvascular Complications'].apply(merge_categories)
 
-plt.figure(figsize=(15, 10))
+plt.figure(figsize=(8, 6))
 sns.boxplot(x='Microvascular Complications', y='Fasting Plasma Glucose (mg/dl)', data=cd1)
 plt.title('Fasting Plasma Glucose by Presence of Microvascular Complications')
 plt.xticks(rotation=45, ha='right')
@@ -291,11 +291,11 @@ plt.show()
 # and not far enough to be measured in a seperate quartile. Even though it still contains a single outlier on the high end.
 
 
-# In[25]:
+# In[64]:
 
 
 # Visualization 4: Line Plot of HbA1c Levels Over Time
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(8, 6))
 sns.lineplot(x='Merge DoD (years)', y='HbA1c (mmol/mol)', data=cd1)
 plt.title('HbA1c Levels Over Time')
 plt.xlabel('Duration of Diabetes in Years')
@@ -309,7 +309,7 @@ plt.show()
 #  If we would rather not then we can use 'errorbar=None' and it will remove the light blue shading 
 
 
-# In[26]:
+# In[65]:
 
 
 # how to get the labels under BMI
@@ -322,7 +322,7 @@ print(unique_labels)
 cd1['Diabetic Macrovascular  Complications'].value_counts()
 
 
-# In[27]:
+# In[66]:
 
 
 # Create a sample DataFrame with the given unique labels
@@ -349,7 +349,7 @@ replacement_mapping = {'peripheral arterial disease, cerebrovascular disease': '
 cd1['Diabetic Macrovascular Complications'] = cd1['Diabetic Macrovascular  Complications'].replace(replacement_mapping)
 
 
-# In[28]:
+# In[67]:
 
 
 # Define a function to merge and relabel the categories
@@ -366,11 +366,12 @@ cd1['Merged Macrovascular Complications'] = cd1['Diabetic Macrovascular Complica
 print(cd1['Merged Macrovascular Complications'].unique())
 
 
-# In[34]:
+# In[68]:
 
 
 #Correlation Between BMI and Diabetic Macrovascular Complications: 
 #Explore the correlation between BMI and the occurrence of macrovascular complications using a bar graph.
+plt.figure(figsize=(8, 6))
 cd1.groupby('Merged Macrovascular Complications')['BMI (kg/m2)'].mean().plot(kind='bar', color='cyan')
 plt.xlabel('Macrovascular Complications')
 plt.xticks(rotation=45, ha='right')
@@ -380,11 +381,11 @@ plt.tight_layout()
 plt.savefig('bmidmc.png')
 
 
-# In[30]:
+# In[69]:
 
 
 # Visualization 5: Histogram of Age Distribution by Microvascular Complications
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(8, 6))
 sns.histplot(data=cd1, x='Age (years)', hue='Microvascular Complications', multiple='stack')
 plt.title('Age Distribution by Microvascular Complications')
 plt.savefig('histogram.png')
@@ -395,7 +396,7 @@ plt.show()
 # while the majority of patients 55-70 had most if not all of the issues associated with Microvascular Complications
 
 
-# In[31]:
+# In[77]:
 
 
 # Visualization 6: Heatmap of Correlation Matrix
@@ -405,7 +406,7 @@ df_numeric = cd1.select_dtypes(include=['number'])
 # Calculate the correlation matrix
 correlation_matrix = df_numeric.corr()
 # Plot the heatmap of the correlation matrix with enhancements
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(8, 6))
 sns.heatmap(correlation_matrix, annot=False, cmap='viridis', fmt='.2f', linewidths=0.5, linecolor='gray')
 plt.title('Correlation Matrix', fontsize=16)
 plt.xticks(rotation=45, ha='right')
@@ -419,14 +420,14 @@ plt.show()
 # 
 
 
-# In[32]:
+# In[71]:
 
 
 # Convert numeric values in 'Gender (Female=1, Male=2)' column to meaningful labels
 cd1['Gender (Female=1, Male=2)'] = cd1['Gender (Female=1, Male=2)'].map({1: 'Female', 2: 'Male'})
 
 
-# In[36]:
+# In[72]:
 
 
 # Visualization 8: Stacked Bar Chart of Diabetic Macrovascular Complications by Gender and HbA1c Levels
@@ -440,7 +441,7 @@ cd1['HbA1c Level Range'] = pd.cut(cd1['HbA1c (mmol/mol)'], bins=bin_edges, label
 cd1['Gender (Female=1, Male=2)']= cd1['Gender (Female=1, Male=2)'].astype(str)
 
 # Plot the stacked bar chart using the HbA1c level ranges
-plt.figure(figsize=(20, 12))
+plt.figure(figsize=(8, 6))
 sns.countplot(x='HbA1c Level Range', hue='Gender (Female=1, Male=2)', data=cd1)
 plt.title('Diabetic Macrovascular Complications by Gender and HbA1c Levels')
 plt.xlabel('HbA1c Level Range (mmol/mol)')
